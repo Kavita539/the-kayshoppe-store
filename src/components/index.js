cc5658f5f67d84e43fefacffecd8afaed723740e
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-03-25 23:38:58 +0530

    feat : cart & wishlist created

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { Footer } from "./footer/Footer";
 export { CategoryCard } from "./category-feature/CategoryCard";
 export { HorizontalCard } from "./horizontal-card/HorizontalCard";
 export { VerticalCard } from "./vertical-card/VerticalCard";
+export { CartCard } from "./cart-card/CartCard";
+export { WishlistCard } from "./wishlist-card/WishlistCard";
