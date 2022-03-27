7fdca2ae1c026ce335d9334f82f2174a0df720c7
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-03-27 16:30:55 +0530

    refactor : category feature

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,35 +1,52 @@
 import "./home.css";
-import { CategoryCard, Header, Footer, HorizontalCard } from "../../components";
+import { CategoryCard, Header, Footer, HorizontalCard, Loader } from "../../components";
 import { Link } from "react-router-dom";
-import { useCategory } from "../../context/categoryContext";
+import { useCategory, useProducts } from "../../context";
+
 
 const Home = ()=>{
-    
-    return(
-        <>
-        <Header/>
-        {/* Banner section for the e-commerce */}
-          <div className="banner flex-center" style={{ backgroundImage: `url(https://www.pngmagic.com/product_images/ecommerce-website-banner.jpg)` }}>
-              
-                <div className="banner-content text-center">
-                    <h2 className="banner-heading"> <span className="banner-initials custom-color">K</span>ayy<span
-                            className="banner-initials custom-color">S</span>hoppe</h2>
-                    <p className="banner-sub-heading custom-color">ITS KAYY TO SPLURGE!</p>
-                    <div className="banner-cta">
-                        <Link to="/products" className="btn btn-primary">Shop now</Link>
-                        <a href="#categories" className="btn outline-btn-primary">Categories</a>
-                    </div>
-                </div>
-            </div>
-            
-            {/* Category section */}
+const { categories, loader: categoryLoader, error: categoryError } = useCategory();
+// const {products, loader:productLoader, error:productError} = useProducts();
 
-            <CategoryCard/>
-        
-            <HorizontalCard />
-            <Footer />
-        </>
-    );
+return(
+<>
+    <Header />
+    {/* Banner section for the e-commerce */}
+    <div className="banner flex-center"
+        style={{ backgroundImage: `url(https://www.pngmagic.com/product_images/ecommerce-website-banner.jpg)` }}>
+
+        <div className="banner-content text-center">
+            <h2 className="banner-heading"> <span className="banner-initials custom-color">K</span>ayy<span
+                    className="banner-initials custom-color">S</span>hoppe</h2>
+            <p className="banner-sub-heading custom-color">ITS KAYY TO SPLURGE!</p>
+            <div className="banner-cta">
+                <Link to="/products" className="btn btn-primary">Shop now</Link>
+                <a href="#categories" className="btn outline-btn-primary">Categories</a>
+            </div>
+        </div>
+    </div>
+
+    {/* Category section */}
+    <section className="text-center" id="categories">
+        <h1 className="section-heading">Categories</h1>
+
+        <div className="category-area">
+            {categoryLoader &&
+            <Loader />}
+            {categoryError && <div>{categoryError}</div>}
+            {categories &&
+            categories.map(({ _id, categoryName, image }) => (
+            <CategoryCard key={_id} category={categoryName} categoryImage={image} />
+            ))}
+
+        </div>
+    </section>
+
+
+    <HorizontalCard />
+    <Footer />
+</>
+);
 }
 
 export { Home };
