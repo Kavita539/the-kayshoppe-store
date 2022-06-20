c4b85be06497c34641f6d99c9f67cf1456198d33
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-06-20 13:28:29 +0530

    feat: conditional rendering

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,46 +3,53 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../context";
 
 const Header = () => {
-    const { state: user, logout } = useAuth();
+const { state: user, logout } = useAuth();
 
-    return(
-        <nav className="navbar">
-                <div className="left-navbar">
-                    <button id="menu-icon-button" className="burger-menu-button navlist-link-item">
-                        <i className="fas fa-bars"></i>
-                    </button>
-                    <Link to="/" className="link-no-style" >
-                        <div className="nav-logo-title"> <span className="text-xl custom-color">K</span>ayy<span
-                                className="text-xl custom-color">S</span>hoppe</div>
-                        <div className="nav-logo-tagline custom-color">ITS KAYY TO SPLURGE!</div>
-                    </Link>
-                </div>
-                <div className="mid-navbar">
-                    <div className="nav-search-bar">
-                        <button className="btn search-btn"><i className="fas fa-search"></i></button>
-                        <input className="form-field" type="text" placeholder="search here..." />
-                    </div>
-                </div>
-                <ul className="right-navbar">
-                <li>
-                        <Link to="/products" className="navlist-link-item"> <button
-                                className="btn link-btn">Shop Now</button></Link>
-                    </li>
-                    <li>
-                        <Link to="/wishlist" className="navlist-link-item"> <i
-                                className="fas fa-heart fa-md"></i></Link>
-                    </li>
-                    <li>
-                        <Link to="/cart" className="navlist-link-item"> <i
-                                className="fas fa-shopping-cart"></i></Link>
-                    </li>
-                    <li>
-                        <Link to="/signin" className="navlist-link-item"> <i
-                                className="fas fa-user"></i></Link>
-                    </li>
-                </ul>
-            </nav>
-    );
+return(
+<nav className="navbar">
+    <div className="left-navbar">
+        <button id="menu-icon-button" className="burger-menu-button navlist-link-item">
+            <i className="fas fa-bars"></i>
+        </button>
+        <Link to="/" className="link-no-style">
+        <div className="nav-logo-title"> <span className="text-xl custom-color">K</span>ayy<span
+                className="text-xl custom-color">S</span>hoppe</div>
+        <div className="nav-logo-tagline custom-color">ITS KAYY TO SPLURGE!</div>
+        </Link>
+    </div>
+    <div className="mid-navbar">
+        <div className="nav-search-bar">
+            <button className="btn search-btn"><i className="fas fa-search"></i></button>
+            <input className="form-field" type="text" placeholder="search here..." />
+        </div>
+    </div>
+    <ul className="right-navbar">
+        <li>
+            <Link to="/products" className="navlist-link-item"> <button className="btn link-btn">Shop Now</button>
+            </Link>
+        </li>
+        <li>
+            <Link to="/wishlist" className="navlist-link-item"> <i className="fas fa-heart fa-md"></i></Link>
+        </li>
+        <li>
+            <Link to="/cart" className="navlist-link-item"> <i className="fas fa-shopping-cart"></i></Link>
+        </li>
+        {user.token ? (
+        <li>
+            <button className="btn outline-btn logout-btn" onClick={()=> logout()}>
+                LogOut
+            </button>
+        </li>
+        ) : (
+        <li>
+            <Link to="/signin" className="navlist-link-item"> <i className="fas fa-user"></i></Link>
+
+        </li>
+        )}
+
+    </ul>
+</nav>
+);
 };
 
 export { Header };
